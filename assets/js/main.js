a77508c62d51a2a75bee4db9a14b9ef9bf351483
Efraim Rocha <67542881+efraimrocha@users.noreply.github.com>
2024-02-06 21:54:50 -0300

    Fixes bug - leftover comma.
    chains the join function to the end of the block.

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,14 +58,14 @@ function professionalExperience(profileData){
     const experience = document.getElementById('profile.experience')
     experience.innerHTML = profileData.professionalExperience.map(experience => {
         return`
-        <li>
-            <h3 class="title">${experience.name}</h3>
-            <p class="period">${experience.period}</p>
-        <p>
-            ${experience.description}
-        </p>
+            <li>
+                <h3 class="title">${experience.name}</h3>
+                <p class="period">${experience.period}</p>
+            <p>
+                ${experience.description}
+            </p>
         `
-    })
+    }).join('')
 
 }
 
